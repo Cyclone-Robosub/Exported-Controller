8159a7e72640daa2286b65ecbbf31d4a434fd9cd
ZenynEthridge <zenyn.ethridge@gmail.com>
2025-04-23 18:59:04 -0700

    Improved cmakelists, some other stuff

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 #include "ControllerForExport.h"
 
 int main(){
-        std::vector<double> inputs = {1.0, 2.0, 3.0, 2.0, 1.0, 0.0, -1.0, -2.0};
+    std::vector<double> inputs = {
+        1.0, 2.0, 3.0, 2.0, 1.0, 0.0, -1.0, -2.0};
     std::vector<double> outputs;
-
+    
+//  ControllerForExport controller;
         
     std::cout << "Testing!\n";
     return 0;
