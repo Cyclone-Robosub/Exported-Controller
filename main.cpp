76fd86d818ba1471c25fe9c493ef85a95db0427f
zenyn <zenyn.ethridge@gmail.com>
2025-04-26 20:47:59 -0700

    minor progress on cmake errors

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
         1.0, 2.0, 3.0, 2.0, 1.0, 0.0, -1.0, -2.0};
     std::vector<double> outputs;
     
-//  ControllerForExport controller;
+    ControllerForExport controller;
         
     std::cout << "Testing!\n";
     return 0;
